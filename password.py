a32a301f8ad210b3926e3bb7b84cb3d1ff51bf21
jvictortech <joaoinformaticaatm@gmail.com>
2023-08-25 19:29:14 -0300

    primeiro commit

---
+++ password.py
@@ -0,0 +1,22 @@
+import random
+import PySimpleGUI as sg
+import os
+
+class Gerarsenha:
+    def __init__(self):
+        sg.theme('Black')
+        layout = [
+           [sg.Text ] 
+        ]
+        pass
+    def Inicio(self):
+        pass
+    def salvar_senha(self):
+        pass
+
+senha = Gerarsenha()
+senha.Inicio()
+
+
+
+
