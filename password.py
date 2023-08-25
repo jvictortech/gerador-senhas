c6e8bef7b3050cf22f6274c82e09e021bd6f4f8e
jvictortech <joaoinformaticaatm@gmail.com>
2023-08-25 19:30:40 -0300

    restante do código

--- password.py
+++ password.py
@@ -6,9 +6,15 @@ class Gerarsenha:
     def __init__(self):
         sg.theme('Black')
         layout = [
-           [sg.Text ] 
+           [sg.Text('Nome', size= (10, 1)),
+            sg.Input(key='nome', size =(20, 1)),
+            sg.Text('E-mail/Usuário', size = (10,1)), sg.Input(key='usuario', size=(20, 1))],
+            sg.Text('Quantidade de caracteres'), sg.Combo(values=list(
+            range(30)), ke='total_chars', default_value=1, size=(3,1)),
+            [sg.Output(size=(32,5))],
+            [sg.Button('GERAR')]
         ]
-        pass
+       self.janela = sg.Window('Gerador de Senhas', layout)
     def Inicio(self):
         pass
     def salvar_senha(self):
